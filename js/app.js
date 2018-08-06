28aa64a02812fed1758db857a0b804287dbfd5af
Shane Lester <Shanejlester@gmail.com>
2018-08-06 01:35:32 -0400

    Add ability to place bugs on screen at beginning of three squares

--- js/app.js
+++ js/app.js
@@ -1,12 +1,18 @@
 // Enemies our player must avoid
 class Enemy {
-    constructor() {
+    constructor(theLocation = [0,220] , theSpeed = 1) {
         // Variables applied to each of our instances go here,
         // we've provided one for you to get started
 
         // The image/sprite for our enemies, this uses
         // a helper we've provided to easily load images
         this.sprite = 'images/enemy-bug.png';
+
+        this.x = theLocation[0];
+
+        this.y = theLocation[1];
+
+        this.speed = theSpeed;
 
     }
 
@@ -22,7 +28,24 @@ class Enemy {
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     };
-}
+
+    static getLocations(locNum) {
+        let location = [];
+        // Bottom Square
+        if (locNum === 0) {
+            location = [0,220];
+        } 
+        // Middle Square
+        else if (locNum === 1) {
+            location = [0,140];
+        }
+        // Top Square
+        else {
+            location = [0,60];
+        }
+        return location;
+    }
+};
 // var Enemy = function() {
 //     // Variables applied to each of our instances go here,
 //     // we've provided one for you to get started
@@ -66,7 +89,11 @@ class Player {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-let allEnemies = [];
+const enemy1 = new Enemy(Enemy.getLocations(0));
+const enemy2 = new Enemy(Enemy.getLocations(1));
+const enemy3 = new Enemy(Enemy.getLocations(2));
+
+let allEnemies = [enemy1, enemy2, enemy3];
 let player = new Player();
 
 
